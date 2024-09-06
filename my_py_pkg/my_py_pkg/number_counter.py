23b36b3e49fa81f94a94ce5d5f1c7fd89d83a7ee
JoaoGaBRiel-X <joaogabriel.mc@hotmail.com>
2024-09-05 21:51:05 -0300

    Aula ROS2 Services - Client/Server

--- my_py_pkg/my_py_pkg/number_counter.py
+++ my_py_pkg/my_py_pkg/number_counter.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
 
 from example_interfaces.msg import Int64
+from example_interfaces.srv import SetBool
 
 class NumberCounterNode(Node):
     def __init__(self):
@@ -10,6 +11,7 @@ class NumberCounterNode(Node):
         self.counter_ = 0
         self.publisher_ = self.create_publisher(Int64, "number_count", 10)
         self.subscriber_ = self.create_subscription(Int64, "number", self.callback_number, 10)
+        self.reset_counter_service_ = self.create_service(SetBool, "reset_counter", self.callback_reset_number_count)
         self.get_logger().info("Number Counter Publisher has been started.")
 
 
@@ -20,6 +22,17 @@ class NumberCounterNode(Node):
         new_msg.data = self.counter_
         self.publisher_.publish(new_msg)
 
+    def callback_reset_number_count(self, request, response):
+        if request.data == True:
+            self.counter_ = 0
+            self.get_logger().info("Number Counter Zerado!")
+            response.success = True
+            response.message = "Number counter zerado com sucesso!"
+        else:
+            response.success = False
+            response.message = "Serviço chamado mas não foi zerado o contador!"
+        
+        return response
 
 
 def main(args=None):
